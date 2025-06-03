b80a184c9a1d9758643a84c45998a9b59af3f2ba
Nosochekir <kperov15@gmail.com>
2025-06-03 12:28:59 +0300

    fixed lesson 5

--- accelerometer/src/main/java/ru/mirea/perovka/accelerometer/MainActivity.java
+++ accelerometer/src/main/java/ru/mirea/perovka/accelerometer/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.mirea.perovka.accelerometer;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -7,57 +8,58 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView azimuthTextView;
+    private TextView pitchTextView;
+    private TextView rollTextView;
     private SensorManager sensorManager;
-    private	Sensor accelerometer;
-    private	TextView azimuthTextView;
-    private	TextView pitchTextView;
-    private	TextView rollTextView;
+    private Sensor accelerometerSensor;
+
     @Override
-    protected void onCreate(Bundle	savedInstanceState)	{
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v,	insets)	->	{
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left,	systemBars.top,	systemBars.right,	systemBars.bottom);
-            return	insets;
-        });
-        sensorManager =	(SensorManager)	getSystemService(SENSOR_SERVICE);
-        accelerometer =	sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, accelerometer,	SensorManager.SENSOR_DELAY_NORMAL);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
         azimuthTextView = findViewById(R.id.textViewAzimuth);
         pitchTextView = findViewById(R.id.textViewPitch);
         rollTextView = findViewById(R.id.textViewRoll);
     }
+
     @Override
-    protected void onPause()	{
+    protected void onPause() {
         super.onPause();
         sensorManager.unregisterListener(this);
     }
+
     @Override
-    protected void onResume()	{
+    protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, accelerometer,
-                SensorManager.SENSOR_DELAY_NORMAL);
-    }
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if	(event.sensor.getType()	== Sensor.TYPE_ACCELEROMETER)	{
-            float x	= event.values[0];
-            float y	= event.values[1];
-            float z	= event.values[2];
-            azimuthTextView.setText(String.format("Azimuth: %s", x));
-            pitchTextView.setText(String.format("Pitch: %s", y));
-            rollTextView.setText(String.format("Roll: %s", z));
+        if (accelerometerSensor != null) {
+            sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
+
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float valueAzimuth = event.values[0];
+            float valuePitch = event.values[1];
+            float valueRoll = event.values[2];
+
+
+            azimuthTextView.setText("Azimuth: " + valueAzimuth);
+            pitchTextView.setText("Pitch: " + valuePitch);
+            rollTextView.setText("Roll: " + valueRoll);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
